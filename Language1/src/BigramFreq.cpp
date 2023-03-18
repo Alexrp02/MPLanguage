10b4986cbbfc9d19215d3f22d2cdad5973967d3a
AlexRP <aleeexrp02@gmail.com>
2023-03-18 13:10:44 +0100

    Implementado bigramfreq en cpp

--- Language1/src/BigramFreq.cpp
+++ Language1/src/BigramFreq.cpp
@@ -7,6 +7,58 @@
 
 #include "BigramFreq.h"
 
+/**
+ * @brief Base constructor. It builds a BigramFreq object with "__" as 
+ * the text of the bigram and 0 as the frequency
+ */
 BigramFreq::BigramFreq() {
+    _bigram = Bigram('_', '_');
+    _frequency = 0;
 }
 
+/**
+ * @brief Gets a const reference to the Bigram of this BigramFreq object.
+ * Query method
+ * @return A const reference to the Bigram of this BigramFreq object
+ */
+Bigram BigramFreq::getBigram() {
+    return _bigram;
+}
+
+/**
+ * @brief Gets the frequency of this BigramFreq object
+ * Query method
+ * @return The frequency of this BigramFreq object
+ */
+int BigramFreq::getFrequency() {
+    return _frequency;
+}
+
+/**
+ * @brief Sets the Bigram of this BigramFreq object
+ * Modifier method
+ * @param bigram The new Bigram value for this object. Input parameter
+ */
+void BigramFreq::setBigram(Bigram bigram) {
+    _bigram = bigram ;
+}
+
+/**
+ * @brief Sets the frequency of this BigramFreq object. Modifier method.
+ * @throw std::out_of_range if @p frequency is negative
+ * @param frequency the new frequency value for this BigramFreq object. 
+ * Input parameter
+ */
+void BigramFreq::setFrequency(int frequency) {
+    _frequency = frequency ;
+}
+
+/**
+ * @brief Obtains a string with the string and frecuency of the bigram
+ * in this object (separated by a whitespace). Query method
+ * @return A string with the string and frecuency of the bigram
+ * in this object. 
+ */
+std::string BigramFreq::toString() {
+    return (_bigram.toString() + " " + std::to_string(_frequency)) ;
+}
