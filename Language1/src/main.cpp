d1e52db58bab6ae1fecec6d74799cdb000792d56
AlexRP <aleeexrp02@gmail.com>
2023-03-19 19:39:41 +0100

    Añadida lectura de bigramas y frecuencia.

--- Language1/src/main.cpp
+++ Language1/src/main.cpp
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <stdio.h> 
 #include "Bigram.h"
+#include "BigramFreq.h"
 using namespace std ;
 
 /**
@@ -29,61 +30,24 @@ using namespace std ;
  * > language0 < data/SimpleText.txt
  */
 int main(int argc, char* argv[]) {
-    // This string contains the list of characters that are considered as
-    // valid within a word. The rest of characters are considered as
-    // separators
-    const std::string validCharacters = "abcdefghijklmnopqrstuvwxyz\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF";
-    const int TAM = 100 ;
+    int tam ; 
+    cin >> tam ;
+    BigramFreq array[tam] ;
     
-    // Read a text
-    string texto = "" ;
-    cin >> texto ;
-    cout << " ----- INPUT TEXT -----" << endl ;
-    cout << texto << endl ;
-    
-    // Read a bigram (tex with two characters)
-    string string_input = "" ;
-    cin >> string_input ;
-    Bigram bigram_input = Bigram(string_input) ;
-    cout << " ----- INPUT BIGRAM FOR CONVERSION ----- " << endl ;
-    cout << bigram_input.toString() << endl ;
-    
-    // Find the bigrams in text and put them in an array of Bigrams
-    int util = 0 ;
-    Bigram array[TAM] ;
-    
-    for (int i=0; i<texto.size()-1 ; i++) {
-        char caracter1, caracter2 ;
-        Bigram nuevo ;
-        if(isValidCharacter(tolower(texto.at(i)), validCharacters)){
-            caracter1 = tolower(texto.at(i)) ;
-                if (isValidCharacter(tolower(texto.at(i+1)), validCharacters)){
-                    caracter2 = tolower(texto.at(i+1)) ;
-                    nuevo = Bigram(caracter1, caracter2) ;
-                    array[util++] = nuevo ;
-                }
-            }
-        }
-    
-    // Show the bigrams stored in the array
-    cout << " ----- BIGRAMS BEFORE CONVERSION ----- " << endl;
-    cout << "LENGTH=" << util << endl ;
-    for (int i=0 ; i<util ; i++) {
-        cout << array[i].toString() << endl ;
+    for (int i=0 ; i<tam ; i++) {
+        string text ;
+        int freq ;
+        BigramFreq b ;
+        cin >> text ;
+        cin >> freq ;
+        b.setBigram(Bigram(text)) ;
+        b.setFrequency(freq) ;
+        array[i] = b ;
     }
     
-    // Convert to uppercase the bigrams in the array that are equals to input bigram
-    for (int i=0 ; i<util ; i++){
-        if(array[i].toString() == bigram_input.toString()) {
-            array[i].toUpper() ;
-        }
-    }
-    
-    // Show again the bigrams stored in the array
-    cout << " ----- BIGRAMS AFTER CONVERSION ----- " << endl;
-    cout << "LENGTH=" << util << endl ;
-    for (int i=0 ; i<util ; i++) {
-        cout << array[i].toString() << endl ;
+    cout << tam << endl ;
+    for (int i=0 ; i<tam ; i++) {
+        cout << array[i].getBigram().toString() << " " << array[i].getFrequency() << endl ;
     }
 }
 
